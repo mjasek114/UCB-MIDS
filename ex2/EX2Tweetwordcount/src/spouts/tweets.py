6582f7d94c10d1feddca824a92d3dcda34200211
root <root@ip-172-31-50-248.ec2.internal>
2015-11-27 11:30:53 +0000

    updated bolts, spouts, topology

--- ex2/EX2Tweetwordcount/src/spouts/tweets.py
+++ ex2/EX2Tweetwordcount/src/spouts/tweets.py
@@ -10,10 +10,10 @@ from streamparse.spout import Spout
 # Twitter credentials
 ################################################################################
 twitter_credentials = {
-    "consumer_key"        :  "<enter your consumer key>",
-    "consumer_secret"     :  "<enter your consumer secret key>",
-    "access_token"        :  "<enter your access token>",
-    "access_token_secret" :  "<enter your access token secret key>",
+    "consumer_key"        :  "YAMNPe4Ao1jlHr5bq08aYtOR6",
+    "consumer_secret"     :  "XcXA5qjWbeaEE5puVYZN7PfRj9dRRByYLnUBuzfkjlGlWDwQL7",
+    "access_token"        :  "97372015-0nwwZaSoyv90bOoNG0MlpmKNEGag3C7LcuDfTKIYg",
+    "access_token_secret" :  "5favVcQfRujhC8pP5cdtqGmO89BySKLRmJG7kjbRBwVy2",
 }
 
 def auth_get(auth_key):
@@ -63,13 +63,17 @@ class Tweets(Spout):
         stream = tweepy.Stream(auth, listener, timeout=None)
         stream.filter(languages=["en"], track=["a", "the", "i", "you", "u"], async=True)
 
+        self.sentences = [ "She advised him to take a long holiday, so he immediately quit work and took a trip around the world", "I was very glad to get a present from her", "he will be here in half an hour", "She saw him eating a sandwich", ]
+        self.sentences = itertools.cycle(self.sentences)
+#        self.count = 0
+
     def queue(self):
         return self._queue
 
     def tweepy_api(self):
         return self._tweepy_api
 
-    def next_tuple(self):
+    def next_tuple2(self):
         try:
             tweet = self.queue().get(timeout = 0.1) 
             if tweet:
@@ -79,6 +83,12 @@ class Tweets(Spout):
         except Queue.Empty:
             self.log("Empty queue exception ")
             time.sleep(0.1) 
+
+    def next_tuple(self):
+ #       if self.count < 144:
+            tweet = next(self.sentences)
+            self.emit([tweet])
+ #           self.count += 1
 
     def ack(self, tup_id):
         pass  # if a tuple is processed properly, do nothing
