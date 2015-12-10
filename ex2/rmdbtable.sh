b7ae16494d1cdac109c50e278a612f9af18211f5
root <root@ip-172-31-54-64.ec2.internal>
2015-12-10 06:55:41 +0000

    removed ex2 folder

--- ex2/rmdbtable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# create a new database call tcount and a new table called tweetwordcount
-psql -U postgres -c 'DROP DATABASE tcount'
-psql -U postgres -c 'CREATE DATABASE tcount'
-psql -U postgres -d tcount -c 'DROP TABLE tweetwordcount'
-psql -U postgres -d tcount -c 'CREATE TABLE tweetwordcount (word TEXT PRIMARY KEY NOT NULL, count INT NOT NULL)'
